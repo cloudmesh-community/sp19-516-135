0d53043b7ed81a1fb6c7d2e57c5f8ba3eb04a5e6
xiao yue <xiaoyue@149-160-197-242.dhcp-bl.indiana.edu>
2019-04-19 12:09:56 -0400

    finish test file

--- project-code/cloudmesh-keys/tests/test_config.py
+++ project-code/cloudmesh-keys/tests/test_config.py
@@ -1,12 +1,6 @@
 """ run with
 
-python setup.py install; nosetests -v --nocapture tests/test_config.py:Test_configdict.test_001
-
-nosetests -v --nocapture tests/test_config.py
-
-or
-
-nosetests -v tests/test_config.py
+pytest -v --capture=no tests/test_config.py
 
 """
 from __future__ import print_function
@@ -15,47 +9,32 @@ import os
 
 from cloudmesh.config2.api.encryption import EncryptFile
 
+import pytest
+
 # noinspection PyMethodMayBeStatic,PyMethodMayBeStatic,PyMethodMayBeStatic,PyPep8Naming,PyBroadException,PyBroadException
+
+@pytest.mark.incremental
 class Test_configdict:
-    root_path = os.path.abspath(os.sep)
-    cwd_path = os.getcwd()
 
-    def setup(self):
-        # os.system("cm help")
-        # self.etc_yaml = os.path.join(self.cwd_path, "cloudmesh_client", "etc", "cloudmesh.yaml")
-        self.tmp_yaml = os.path.join(self.root_path, "tmp", "cloudmesh.yaml")
-        self.tmp_dir = os.path.join(self.root_path, "tmp")
-        pass
 
-    # noinspection PyPep8Naming
-    def tearDown(self):
-        pass
 
-    def test_config(self):
+    def setup_class(self):
+        self.e = EncryptFile('~/.cloudmesh/cloudmesh.yaml', '~/.cloudmesh/cloudmesh.yaml.enc', '')
 
-        '''
-        for filename in ['file.txt', 'secret.txt']:
-            try:
-                os.remove(filename)
-            except FileNotFoundError:
-                pass
 
-        # Creating a file with data
+    def test_getRandonPassword(self):
+        self.e.getRandonPassword()
 
-        with open("file.txt", "w") as f:
-            f.write("Big Data is here.")
-        '''
-        e = EncryptFile('/Users/xiaoyue/Desktop/test.yaml', '/Users/xiaoyue/Desktop/test.yaml.enc', '')
-        e.decrypt()
+    def test_encrypt(self):
+        self.e.encrypt()
 
-        '''
-        # if the file is existed
-        file_in = "secret.txt"
-        file_out = "plain.txt"
-        # if the file is existed
+    def test_encryptPassword(self):
+        self.e.encryptPassword()
 
-        if not os.path.exists(file_in):
-            os.system(r"touch {}".format(file_out))  # create the file
+    def test_decryptRandomKey(self):
+        self.e.decryptRandomKey()
 
-        '''
+    def test_decrypt(self):
+        self.e.decrypt()
 
+        
